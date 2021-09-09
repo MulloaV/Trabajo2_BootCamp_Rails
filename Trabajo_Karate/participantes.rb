bef1738d95c91cb45325983752ebd1de67a49576
MulloaV <mulloa0179@e-camp.cl>
2021-09-09 19:50:11 -0300

    Cambios
    Cambios y más cambios

--- Trabajo_Karate/participantes.rb
+++ Trabajo_Karate/participantes.rb
@@ -1,8 +1,8 @@
-class Participantes
+class Participantes #{}< Torneo
     
-	attr_accessor :codigo, :nombre, :edad, :dni, :colorCinturon, :partidasGanadas, :partidasEmpatadas, :partidasPerdidas
-	def initialize codigo, nombre, edad, dni, colorCinturon,partidasGanadas, partidasEmpatadas, partidasPerdidas
-		@codigo= codigo
+	attr_accessor :codigo, :nombre, :edad, :dni, :colorCinturon, :partidasGanadas, :partidasEmpatadas, :partidasPerdidas, :codEquipo
+	def initialize codigo, nombre, edad, dni, colorCinturon,partidasGanadas, partidasEmpatadas, partidasPerdidas, codEquipo
+		@codigo = codigo
         @nombre = nombre
 		@edad = edad
 		@dni = dni
@@ -10,10 +10,15 @@ class Participantes
 		@partidasGanadas = partidasGanadas
 		@partidasEmpatadas = partidasEmpatadas
 		@partidasPerdidas = partidasPerdidas
+		@codEquipo = codEquipo
         @puntaje =  (@partidasGanadas) * 4 + (@partidasEmpatadas) * 3
 	end
 
 	def nuevoPuntaje
 		@puntaje = (@partidasGanadas) * 4 + (@partidasEmpatadas) * 3
+	end
+
+	def torneo
+		
 	end
 end    
--- Trabajo_Karate/torneo.rb
+++ Trabajo_Karate/torneo.rb
@@ -9,7 +9,7 @@ class Torneo
       @participantes = Array.new
     end
     
-    def obtener_equipo(codigo)
+    def obtenerEquipo(codigo,nombre)
       for equipo in @equipos
         if equipo.codigo == codigo
           return equipo
@@ -18,39 +18,39 @@ class Torneo
       return nil
     end
     
-    def registrar_equipo(codigo, nombre)
-      equipo = obtener_equipo(codigo)
+    def registrarEquipo(codigo, nombre)
+      equipo = obtenerEquipo(codigo, nombre)
       if equipo == nil
         equipo = Equipo.new(codigo, nombre)
         @equipos.push(equipo)
       end
     end
     
-    def registrar_participante(codigo, nombre, edad, dni, colorCinturon, partidasGanadas, partidasPerdidas, partidasEmpatadas, codEquipo)
-      equipo = obtener_equipo(codEquipo)
+    def registrarParticipante(codigo, nombre, edad, dni, colorCinturon, partidasGanadas, partidasPerdidas, partidasEmpatadas, codEquipo)
+      equipo = obtenerEquipo(codEquipo)
       if equipo != nil 
-        participante = Participante.new(codigo, nombre, edad, dni, colorCinturon, partidasGanadas, partidasPerdidas, partidasEmpatadas)
-        equipo.participantes.push(participante)
-        @participantes.push(participante)
+        participantes = Participantes.new(codigo, nombre, edad, dni, colorCinturon, partidasGanadas, partidasPerdidas, partidasEmpatadas)
+        equipo.participantes.push(participantes)
+        @participantes.push(participantes)
       else
         puts "Equipo no encontrado"
       end
     end
     
-    def obtener_participante(dni) 
-        for participante in @participantes
-          if participante.dni == dni
-            return participante
+    def obtenerParticipantes(dni) 
+        for participantes in @participantes
+          if participantes.dni == dni
+            return participantes
           end
         end
         return nil
     end  
     
-    def actualizar_partidas_ganadas(dni, partidasGanadas)
-      participante = obtener_participante(dni)
+    def actualizarPartidasGanadas(dni, partidasGanadas)
+      participante = obtenerParticipante(dni)
       if participante != nil
         participante.partidasGanadas = partidasGanadas
-        participante.recalcular_puntaje()
+        participante.nuevoPuntaje()
       else
         puts "Participante no encontrado"
       end
@@ -59,19 +59,19 @@ class Torneo
     def ganador()
       maxPuntaje = 0
       campeon = nil
-      for participante in @participantes
-        if participante.puntaje > maxPuntaje
-          maxPuntaje = participante.puntaje
-          campeon = participante
+      for participantes in @participantes
+        if participantes.puntaje > maxPuntaje
+          maxPuntaje = participantes.puntaje
+          campeon = participantes
         end
       end
       return campeon
     end
     
-    def tabla_posiciones()
+    def tablaPosiciones()
       posiciones = []
-      for participante in @participantes
-        posiciones.push(participante)
+      for participantes in @participantes
+        posiciones.push(participantes)
       end
       
       for i in 0...posiciones.count
@@ -87,18 +87,18 @@ class Torneo
       end
       
       for i in 0...posiciones.count
-        participante = posiciones[i]
+        participantes = posiciones[i]
         puts "#{i+1}° lugar:"
         print " "
-        imprimir_info_participante(participante)
+        imprimirInfoParticipante(participantes)
       end
     end
     
-    def listar_equipos()
+    def listarEquipos()
       for equipo in @equipos
-        imprimir_info_equipo(equipo)
+        imprimirInfoEquipo(equipo)
         totalPuntaje = 0
-        for participante in equipo.participantes
+        for participantes in equipo.participantes
           totalPuntaje = totalPuntaje + participante.puntaje
         end
         puts " Puntaje total: #{totalPuntaje}"
@@ -112,17 +112,50 @@ class Torneo
       puts "Equipo: #{equipo.nombre}"
     end
     
-    def imprimir_info_participante(participante)
-      if participante == nil
+    def imprimirInfoParticipantes(participantes)
+      if participantes == nil
         return
       end
-      puts "Participante: #{participante.dni} - #{participante.nombre} de #{participante.edad} años. Puntaje actual : #{participante.puntaje}"   
+      puts "Participante: #{participantes.dni} - #{participantes.nombre} de #{participantes.edad} años. Puntaje actual : #{participante.puntaje}"   
       # puts "    PG: #{participante.partidasGanadas}"
     end
     
   end
 
 
+torneo = Torneo.new
+torneo.registrarEquipo('EQ01', "Karatecas Kids")
+torneo.registrarEquipo("EQ02", "Karatecas de Surco")
+torneo.registrarEquipo("EQ03", "Karatecas de Miraflores")
+torneo.registrarEquipo("EQ04", "Karatecas de San Miguel")
+torneo.registrarEquipo("EQ05", "Karatecas de San Borja")
+torneo.registrarEquipo("EQ06", "Karatecas de La Molina")
+torneo.registrarParticipante("COD01", "Jose Torres", 29, "46531738", "verde", 2, 4, 4, "EQ02")
+torneo.registrarParticipante("COD02", "Emilio Verastegui", 30, "46531740", "verde", 0, 4, 2, "EQ02")
+torneo.registrarParticipante("COD03", "Lorenzo Puente", 31, "46531742", "verde", 0, 4, 1, "EQ02")
+torneo.registrarParticipante("COD04", "Ricardo Rivero", 12, "53173800", "amarillo", 1, 2, 0, "EQ01")
+torneo.registrarParticipante("COD05", "Carlos Herrera", 25, "65331738", "marron", 10, 5, 0, "EQ03")  
 
+# a
+participanteA = torneo.obtenerParticipante("46531738")
+torneo.imprimirInfoParticipante(participanteA)
+# b
+ganadorTorneo = torneo.ganador()
+torneo.imprimirInfoParticipante(ganadorTorneo)
+# c
+torneo.actualizarPartidasGanadas("53173800", 30)
+participanteB = torneo.obtenerParticipante("53173800")
+torneo.imprimirInfoParticipante(participanteB)
+#d
+puts
+torneo.tablaPosiciones()
+#e
+puts
+torneo.listarEquipos()
+  
+  
+  
+  
+  
 
   
